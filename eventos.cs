cb0dee840a8bae6cbce6e1d5d4f0ca10db20e0b2
TonyVazquezzz <43189914+TonyVazquezzz@users.noreply.github.com>
2019-06-13 16:53:02 -0500

    Add files via upload

---
+++ eventos.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+namespace Sistema
+{
+    #region Eventos
+    public class Eventos
+    {
+        #region Member Variables
+        protected int _id;
+        protected string _title;
+        protected string _descripcion;
+        protected string _color;
+        protected string _textColor;
+        protected DateTime _start;
+        protected DateTime _endd;
+        #endregion
+        #region Constructors
+        public Eventos() { }
+        public Eventos(string title, string descripcion, string color, string textColor, DateTime start, DateTime endd)
+        {
+            this._title=title;
+            this._descripcion=descripcion;
+            this._color=color;
+            this._textColor=textColor;
+            this._start=start;
+            this._endd=endd;
+        }
+        #endregion
+        #region Public Properties
+        public virtual int Id
+        {
+            get {return _id;}
+            set {_id=value;}
+        }
+        public virtual string Title
+        {
+            get {return _title;}
+            set {_title=value;}
+        }
+        public virtual string Descripcion
+        {
+            get {return _descripcion;}
+            set {_descripcion=value;}
+        }
+        public virtual string Color
+        {
+            get {return _color;}
+            set {_color=value;}
+        }
+        public virtual string TextColor
+        {
+            get {return _textColor;}
+            set {_textColor=value;}
+        }
+        public virtual DateTime Start
+        {
+            get {return _start;}
+            set {_start=value;}
+        }
+        public virtual DateTime Endd
+        {
+            get {return _endd;}
+            set {_endd=value;}
+        }
+        #endregion
+    }
+    #endregion
+}
---
+++ usu.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+namespace Usuariosreg
+{
+    #region Usu
+    public class Usu
+    {
+        #region Member Variables
+        protected string _Nombre;
+        protected string _Correo;
+        protected string _Contrasena;
+        #endregion
+        #region Constructors
+        public Usu() { }
+        public Usu(string Nombre, string Correo, string Contrasena)
+        {
+            this._Nombre=Nombre;
+            this._Correo=Correo;
+            this._Contrasena=Contrasena;
+        }
+        #endregion
+        #region Public Properties
+        public virtual string Nombre
+        {
+            get {return _Nombre;}
+            set {_Nombre=value;}
+        }
+        public virtual string Correo
+        {
+            get {return _Correo;}
+            set {_Correo=value;}
+        }
+        public virtual string Contrasena
+        {
+            get {return _Contrasena;}
+            set {_Contrasena=value;}
+        }
+        #endregion
+    }
+    #endregion
+}
